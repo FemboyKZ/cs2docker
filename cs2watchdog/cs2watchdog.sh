1d548324bce84e17f3156d49e1db02af35188da5
Szwagi <bygreensky@gmail.com>
2025-02-10 04:24:32 +0000

    Fix typo

--- cs2watchdog/cs2watchdog.sh
+++ cs2watchdog/cs2watchdog.sh
@@ -28,8 +28,8 @@ update_cs2() {
 }
 
 main() {
-    for (( first = 1;; first = 0 )); do
-        [[ first -eq 0 ]] && sleep 10
+    for (( first=1;; first=0 )); do
+        [ $first -eq 0 ] && sleep 10
         
         # The temporary directory might exist if update_cs2 fails
         rm -rf "/repo/tmp" || continue
