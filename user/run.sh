f4c47cbb01ec7f6092db999554a00fc80a48a02a
Dots <dotvnl@gmail.com>
2025-09-19 23:01:56 +0300

    create instead of edit server.cfg, remove extras and fix pw and hostip, more cfgs

--- user/run.sh
+++ user/run.sh
@@ -12,20 +12,19 @@ rm "$server_dir/game/bin/linuxsteamrt64/cs2"
 cp "$build_dir/game/bin/linuxsteamrt64/cs2" "$server_dir/game/bin/linuxsteamrt64/cs2"
 
 # Create server cfg
-cat <<EOF >> "$server_dir/game/csgo/cfg/server.cfg"
+cat <<EOF > "$server_dir/game/csgo/cfg/server.cfg"
 hostname "$HOSTNAME"
-hostip "$IP"
+// hostip 0.0.0.0
 hostport "$PORT"
-sv_password "$PASSWORD"
+sv_password ""
 rcon_password "$RCON_PASSWORD"
 sv_hibernate_when_empty false
 sv_hibernate_postgame_delay 0
 sv_tags "$TAGS"
-exec fkz-settings.cfg
+exec fkz-print.cfg
+// exec fkz-settings.cfg
 exec fkz-logs.cfg
 // exec fkz-tv.cfg
-exec fkz-print.cfg
-mp_restartgame 1
 EOF
 
 # Make sure necessary directories exist
@@ -92,13 +91,10 @@ mv "/tmp/Chat_Logger.json" "$cssharp_cfg_dir/Chat_Logger/Chat_Logger.json"
 jq ". + {DiscordWebhook: \"$DC_CONNECT_WEBHOOK?thread_id=$DC_CONNECT_THREAD\"}" "$cssharp_cfg_dir/ConnectionLogs/ConnectionLogs.json" > "/tmp/ConnectionLogs.json"
 mv "/tmp/ConnectionLogs.json" "$cssharp_cfg_dir/ConnectionLogs/ConnectionLogs.json"
 
-jq ". + {\"GeneralConfig.ServerIP\": \"$IP_DOMAIN:$PORT\", \"EmbedConfig.Title\": \"$HOSTNAME\", \"WebhookConfig.StatusWebhookURL\": \"$DC_STATUS_MSG_WEBHOOK\", \"WebhookConfig.StatusWebhookID\": \"$DC_STATUS_MSG_ID\"}" "$cssharp_cfg_dir/DiscordStatus/DiscordStatus.json" > "/tmp/DiscordStatus.json"
-mv "/tmp/DiscordStatus.json" "$cssharp_cfg_dir/DiscordStatus/DiscordStatus.json"
-
 jq ". + {\"server-api-key\": \"$SVLIST_APIKEY\"}" "$cssharp_cfg_dir/CS2ServerList/CS2ServerList.json" > "/tmp/CS2ServerList.json"
 mv "/tmp/CS2ServerList.json" "$cssharp_cfg_dir/CS2ServerList/CS2ServerList.json"
 
-jq ". + {\"DatabaseParams.User\": \"$DB_USER\", \"DatabaseParams.Password\": \"$DB_PASS\", \"DatabaseParams.Name\": \"$DB_NAME\"}" "$cssharp_cfg_dir/PlayerSettings/PlayerSettings.json" > "/tmp/PlayerSettings.json"
+jq ". + {\"DatabaseParams.Host\": \"$DB_HOST:$DB_PORT\", \"DatabaseParams.User\": \"$DB_USER\", \"DatabaseParams.Password\": \"$DB_PASS\", \"DatabaseParams.Name\": \"$DB_NAME\"}" "$cssharp_cfg_dir/PlayerSettings/PlayerSettings.json" > "/tmp/PlayerSettings.json"
 mv "/tmp/PlayerSettings.json" "$cssharp_cfg_dir/PlayerSettings/PlayerSettings.json"
 
 jq ". + {ApiKey: \"$WS_APIKEY\"}" "$cssharp_cfg_dir/Whitelist/Whitelist.json" > "/tmp/Whitelist.json"
@@ -110,28 +106,31 @@ mv "/tmp/Config.json" "$server_dir/game/csgo/addons/counterstrikesharp/plugins/A
 jq ". + {\"DatabaseUser\": \"$DB_USER\", \"DatabasePassword\": \"$DB_PASS\", \"DatabaseName\": \"$GL_DB_NAME\", \"DatabaseHost\": \"$DB_HOST\", \"Discord.DiscordLogWebhook\": \"$DC_ADMIN_WEBHOOK\"}" "$cssharp_cfg_dir/CS2-SimpleAdmin/CS2-SimpleAdmin.json" > "/tmp/CS2-SimpleAdmin.json"
 mv "/tmp/CS2-SimpleAdmin.json" "$cssharp_cfg_dir/CS2-SimpleAdmin/CS2-SimpleAdmin.json" # theres more webhooks but im too lazy to add them
 
+jq ". + {\"DatabaseUser\": \"$DB_USER\", \"DatabasePassword\": \"$DB_PASS\", \"DatabaseName\": \"$GL_DB_NAME\", \"DatabaseHost\": \"$DB_HOST\", \"Website\": \"$SKINSITE_URL\"}" "$cssharp_cfg_dir/WeaponPaints/WeaponPaints.json" > "/tmp/WeaponPaints.json"
+mv "/tmp/WeaponPaints.json" "$cssharp_cfg_dir/WeaponPaints/WeaponPaints.json" # theres more webhooks but im too lazy to add them
+
 # I like to use metaplugins.ini (created in cs2server/configs.sh) to load plugins, so remove all other vdf files to avoid confusion.
 find "$server_dir/game/csgo/addons/metamod/" -type f -name "*.vdf" -exec rm -f {} +
 
 # Create metaplugins.ini for metamod
 if [[ "${ACCEL,,}" == "true" || "${ACCEL,,}" == "yes" || "$ACCEL" == "1" ]]; then
-    echo "ACCEL addons/AcceleratorCS2/AcceleratorCS2" >> "$server_dir/game/csgo/addons/metamod/metaplugins.ini"
+    echo "ACCEL addons/AcceleratorCS2/AcceleratorCS2" > "$server_dir/game/csgo/addons/metamod/metaplugins.ini"
 else
-    echo ";ACCEL addons/AcceleratorCS2/AcceleratorCS2" >> "$server_dir/game/csgo/addons/metamod/metaplugins.ini"
+    echo ";ACCEL addons/AcceleratorCS2/AcceleratorCS2" > "$server_dir/game/csgo/addons/metamod/metaplugins.ini"
 fi
 
 cat <<EOF >> "$server_dir/game/csgo/addons/metamod/metaplugins.ini"
 ;ACCELCSS addons/AcceleratorCSS/bin/linuxsteamrt64/AcceleratorCSS
-KZ addons/cs2kz/bin/linuxsteamrt64/cs2kz
-CLEANER addons/cleanercs2/cleanercs2
-SQLMM addons/sql_mm/bin/linuxsteamrt64/sql_mm
+;KZ addons/cs2kz/bin/linuxsteamrt64/cs2kz
+;CLEANER addons/cleanercs2/cleanercs2
+;SQLMM addons/sql_mm/bin/linuxsteamrt64/sql_mm
 CSS addons/counterstrikesharp/bin/linuxsteamrt64/counterstrikesharp
-MAM addons/multiaddonmanager/bin/multiaddonmanager
-CCVAR addons/client_cvar_value/client_cvar_value
-LISTFIX addons/serverlistplayersfix_mm/bin/linuxsteamrt64/serverlistplayersfix_mm
+;MAM addons/multiaddonmanager/bin/multiaddonmanager
+;CCVAR addons/client_cvar_value/client_cvar_value
+;LISTFIX addons/serverlistplayersfix_mm/bin/linuxsteamrt64/serverlistplayersfix_mm
 ;VOICEFIX addons/CS2VoiceFix/CS2VoiceFix
 ;GOTVFIX addons/GOTVCrashFix/GOTVCrashFix
-BANFIX addons/gamebanfix/bin/linuxsteamrt64/gamebanfix
+;BANFIX addons/gamebanfix/bin/linuxsteamrt64/gamebanfix
 ;MENUEXPORT addons/MenusExport/bin/MenusExport
 EOF
 
@@ -147,7 +146,7 @@ install_mount() {
 
 install_mount "logs" "logs"
 install_mount "addons/counterstrikesharp/logs" "addons/counterstrikesharp/logs"
-install_mount "addons/counterstrikesharp/plugins/Chat_logger/logs" "addons/counterstrikesharp/plugins/Chat_logger/logs"
+#install_mount "addons/counterstrikesharp/plugins/Chat_logger/logs" "addons/counterstrikesharp/plugins/Chat_logger/logs"
 install_mount "addons/AcceleratorCS2/dumps" "addons/AcceleratorCS2/dumps"
 
 # Run whitelist updater in background if whitelist is enabled
