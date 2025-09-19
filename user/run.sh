3506af9c9866fed65d38fe5d0a3042ffb669aebe
Dots <dotvnl@gmail.com>
2025-09-19 19:16:52 +0300

    hella fixes

--- user/run.sh
+++ user/run.sh
@@ -13,7 +13,7 @@ cp "$build_dir/game/bin/linuxsteamrt64/cs2" "$server_dir/game/bin/linuxsteamrt64
 
 # Create server cfg
 cat <<EOF >> "$server_dir/game/csgo/cfg/server.cfg"
-hostname "$NAME"
+hostname "$HOSTNAME"
 hostip "$IP"
 hostport "$PORT"
 sv_password "$PASSWORD"
@@ -33,27 +33,27 @@ mkdir -p "$server_dir/game/csgo/addons" "$server_dir/game/csgo/tmp"
 
 # Install layers
 install_layer() {
-    cp -rf "$root/layers/$1"/* "$server_dir/game/csgo"
+    cp -rf "/layers/$1"/* "$server_dir/game/csgo"
 }
 
 install_layer "mm"
 rm "$server_dir/game/csgo/addons/metamod/metaplugins.ini"
-sed -i '0,/\t\t\tGame\tcsgo/s//\t\t\tGame\tcsgo\/addons\/metamod\n&/' "$server_dir/game/csgo/gameinfo.gi"
+sed -i "0,/\t\t\tGame\tcsgo/s//\t\t\tGame\tcsgo\/addons\/metamod\n&/" "$server_dir/game/csgo/gameinfo.gi"
 
 install_layer "accel"
 rm "$server_dir/game/csgo/addons/AcceleratorCS2/config.json"
 
 install_layer "kz"
-rm "$server_dir/game/csgo/cfg/cs2kz-server-config.cfg"
+rm "$server_dir/game/csgo/cfg/cs2kz-server-config.txt"
 
-#install_layer "cssharp"
+install_layer "cssharp"
 
-# install_layer "accelcss" # only debug
+# #install_layer "accelcss" # only debug
 
 install_layer "mam"
 rm "$server_dir/game/csgo/cfg/multiaddonmanager/multiaddonmanager.cfg"
 
-install_layer "sqlmm"
+install_layer "sql_mm"
 
 install_layer "ccvar"
 
@@ -64,6 +64,8 @@ install_layer "listfix"
 
 install_layer "banfix"
 
+# install_layer "cssplugins"
+
 install_layer "configs"
 
 # cs2kz cfg (STUPID TXT FILE)
@@ -72,25 +74,31 @@ modify_config() {
     local value="$3"
     sed -i "s/\(\"$key\"[[:space:]]*\)\"[^\"]*\"/\1\"$value\"/" "$1"
 }
-modfiy_config "$server_dir/game/csgo/cfg/cs2kz-server-config.txt" "apiKey" "$CS2KZ_APIKEY"
-modfiy_config "$server_dir/game/csgo/cfg/cs2kz-server-config.txt" "user" "$DB_USER"
-modfiy_config "$server_dir/game/csgo/cfg/cs2kz-server-config.txt" "pass" "$DB_PASS"
+
+modify_config "$server_dir/game/csgo/cfg/cs2kz-server-config.txt" "apiKey" "$CS2KZ_APIKEY"
+modify_config "$server_dir/game/csgo/cfg/cs2kz-server-config.txt" "user" "$DB_USER"
+modify_config "$server_dir/game/csgo/cfg/cs2kz-server-config.txt" "pass" "$DB_PASS"
 
 # cssharp configs
 cssharp_cfg_dir="$server_dir/game/csgo/addons/counterstrikesharp/configs/plugins"
 
-jq ".WebHookURL = '$DC_CHAT_WEBHOOK?thread_id=$DC_CHAT_THREAD'" "$cssharp_cfg_dir/Chat_Logger/Chat_Logger.json"
+jq ". + {WebHookURL: \"$DC_CHAT_WEBHOOK?thread_id=$DC_CHAT_THREAD\"}" "$cssharp_cfg_dir/Chat_Logger/Chat_Logger.json" > "/tmp/Chat_Logger.json"
+mv "/tmp/Chat_Logger.json" "$cssharp_cfg_dir/Chat_Logger/Chat_Logger.json"
 
-jq ".DiscordWebhook = '$DC_CONNECT_WEBHOOK?thread_id=$DC_CONNECT_THREAD'" "$cssharp_cfg_dir/ConnectionLogs/ConnectionLogs.json"
+jq ". + {DiscordWebhook: \"$DC_CONNECT_WEBHOOK?thread_id=$DC_CONNECT_THREAD\"}" "$cssharp_cfg_dir/ConnectionLogs/ConnectionLogs.json" > "/tmp/ConnectionLogs.json"
+mv "/tmp/ConnectionLogs.json" "$cssharp_cfg_dir/ConnectionLogs/ConnectionLogs.json"
 
-jq ".GeneralConfig.ServerIP = '$IP_DOMAIN:$PORT' | .EmbedConfig.Title = '$HOSTNAME'" "$cssharp_cfg_dir/DiscordStatus/DiscordStatus.json"
-jq ".WebhookConfig.StatusWebhookURL = '$DC_STATUS_MSG_WEBHOOK' | .WebhookConfig.StatusWebhookID = '$DC_STATUS_MSG_ID'" "$cssharp_cfg_dir/DiscordStatus/DiscordStatus.json"
+jq ". + {\"GeneralConfig.ServerIP\": \"$IP_DOMAIN:$PORT\", \"EmbedConfig.Title\": \"$HOSTNAME\", \"WebhookConfig.StatusWebhookURL\": \"$DC_STATUS_MSG_WEBHOOK\", \"WebhookConfig.StatusWebhookID\": \"$DC_STATUS_MSG_ID\"}" "$cssharp_cfg_dir/DiscordStatus/DiscordStatus.json" > "/tmp/DiscordStatus.json"
+mv "/tmp/DiscordStatus.json" "$cssharp_cfg_dir/DiscordStatus/DiscordStatus.json"
 
-jq ".server-api-key = '$SVLIST_APIKEY'" "$cssharp_cfg_dir/CS2ServerList/CS2ServerList.json"
+jq ". + {\"server-api-key\": \"$SVLIST_APIKEY\"}" "$cssharp_cfg_dir/CS2ServerList/CS2ServerList.json" > "/tmp/CS2ServerList.json"
+mv "/tmp/CS2ServerList.json" "$cssharp_cfg_dir/CS2ServerList/CS2ServerList.json"
 
-jq ".DatabaseParams.User = '$DB_USER' | .DatabaseParams.Password = '$DB_PASS' | .DatabaseParams.Name = '$DB_NAME'" "$cssharp_cfg_dir/PlayerSettings/PlayerSettings.json"
+jq ". + {\"DatabaseParams.User\": \"$DB_USER\", \"DatabaseParams.Password\": \"$DB_PASS\", \"DatabaseParams.Name\": \"$DB_NAME\"}" "$cssharp_cfg_dir/PlayerSettings/PlayerSettings.json" > "/tmp/PlayerSettings.json"
+mv "/tmp/PlayerSettings.json" "$cssharp_cfg_dir/PlayerSettings/PlayerSettings.json"
 
-jq ".ApiKey = '$WS_APIKEY'" "$cssharp_cfg_dir/Whitelist/Whitelist.json"
+jq ". + {ApiKey: \"$WS_APIKEY\"}" "$cssharp_cfg_dir/Whitelist/Whitelist.json" > "/tmp/Whitelist.json"
+mv "/tmp/Whitelist.json" "$cssharp_cfg_dir/Whitelist/Whitelist.json"
 
 # I like to use metaplugins.ini (created in cs2server/configs.sh) to load plugins, so remove all other vdf files to avoid confusion.
 find "$server_dir/game/csgo/addons/metamod/" -type f -name "*.vdf" -exec rm -f {} +
@@ -117,14 +125,14 @@ BANFIX addons/gamebanfix/bin/linuxsteamrt64/gamebanfix
 ;MENUEXPORT addons/MenusExport/bin/MenusExport
 EOF
 
-# Create symlinks for mounts
-if [[ ! -d "$root/mounts/$ID" ]]; then
-    mkdir -p "$root/mounts/$ID/logs" "$root/mounts/$ID/addons/counterstrikesharp/logs" "$root/mounts/$ID/addons/counterstrikesharp/plugins/Chat_logger/logs"
+# Create folders for mounts if not existing
+if [[ ! -d "/mounts/$ID" ]]; then
+    mkdir -p "/mounts/$ID/logs" "/mounts/$ID/addons/counterstrikesharp/logs" "/mounts/$ID/addons/counterstrikesharp/plugins/Chat_logger/logs"
 fi
 
 install_mount() {
     rm -rf "$server_dir/game/csgo/$2"
-    ln -s "$root/mounts/$ID/$1" "$server_dir/game/csgo/$2"
+    ln -s "/mounts/$ID/$1" "$server_dir/game/csgo/$2"
 }
 
 install_mount "logs" "logs"
@@ -138,4 +146,4 @@ if [[ "${WHITELIST,,}" == "true" || "${WHITELIST,,}" == "yes" || "$WHITELIST" ==
 fi
 
 # Run the server.
-"$server_dir/game/bin/linuxsteamrt64/cs2" -dedicated -ip "$IP" -port "$PORT" -authkey "$WS_APIKEY" +sv_setsteamaccount "$GSLT" +map "$MAP" +mapgroup mg_custom +host_workshop_map "$WS_MAP" +exec server.cfg +game_type 3 +game_mode 0 -maxplayers 64 -nohltv
+"$server_dir/game/cs2.sh" -dedicated -ip "$IP" -port "$PORT" -authkey "$WS_APIKEY" +sv_setsteamaccount "$GSLT" +map "$MAP" +mapgroup mg_custom +host_workshop_map "$WS_MAP" +exec server.cfg +game_type 3 +game_mode 0 -maxplayers 64 -nohltv
